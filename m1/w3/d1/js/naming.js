773c283fa46bb0a72c88f85803b1ee41fa2b92e2
Nabbatrox <alessandrocrescimanni@gmail.com>
2022-07-04 17:05:28 +0200

    d1/esercizio1

---
+++ m1/w3/d1/js/naming.js
@@ -0,0 +1,51 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var broccoli = '18';
+
+var a = 5;
+
+var b = '10';
+
+console.log (broccoli + ' ' + $pet + ' andarono a bere, ma ' + (a * b) + _pet + ' tesero loro una imboscata')
+
+
+
+
+
+var c = (a + b);
+
+
+console.log(a + b);
+console.log(c * a);
+
+var patate = document.getElementsByClassName ("roba blu");
+
+console.log (patate)
+
+patate[0].style.backgroundColor = 'blue';
+patate[2].style.backgroundColor = 'green';
+
+var div = document.getElementsByTagName ('div') 
+
+console.table (div)
+
+
+
+
+div.onclick = function(ciao){'ciao'};
+
+
+
+
