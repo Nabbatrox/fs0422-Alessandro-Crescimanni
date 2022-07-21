42afbb10d1620c09a4ee93884e843118a8750a17
Nabbatrox <66868366+Nabbatrox@users.noreply.github.com>
2022-07-21 16:27:26 +0200

    esercizio 4 d4

---
+++ m2/w1/d4/assets/js/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector ("#aprimenubtn");
+
+
+button.addEventListener("click", function() {
+
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("mostra");
+
+})
+
+  
+
+  
