1c33419497e1253020ffcd71a7ce7244b34bcc46
Nabbatrox <66868366+Nabbatrox@users.noreply.github.com>
2022-07-24 20:03:42 +0200

    fix finale problemi

--- m2/w1/d5/js/memory.js
+++ m2/w1/d5/js/memory.js
@@ -2,20 +2,18 @@ let arrayAnimali = ['🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '�
 //libreria per icone
 //https://html-css-js.com/html/character-codes/
 
-
 let arrayComparison = [];
+var victoryArray = [];
 
 document.body.onload = startGame();
 
 // mi serviranno alcune variabili 1. interval 2. una agganciata alla classe find 
 // 3. una agganciata al'id modal 4. una agganciata alla classe timer
 
-
-let interval;
-let corretto = document.querySelector('.find');
-let gameCompleted = document.querySelector('#modal');
-let tempoAttuale = document.querySelector('.timer');
-
+var interval;
+var corretto = document.querySelector('.find');// A me non e' servito
+var gameCompleted = document.querySelector('#modal');
+var tempoAttuale = document.querySelector('.timer');
 
 //una funzione che serve a mescolare in modo random gli elementi dell'array che viene passato 
 // (l'array contiene le icone degli animali)
@@ -33,106 +31,100 @@ function shuffle(a) {
     return a;
 }
 // una funzione che rimuove la classe active e chiama la funzione startGame()
-
-
-
-
+function playAgain() {
+    gameCompleted.classList.remove('active');
+    gameCompleted.style.display = 'none';
+    startGame();
+}
 
 function startGame() {
 
-            let time = new Date();
+    clearInterval(interval)
 
-            time.setHours(0)
-            time.setMinutes(0)
-            time.setSeconds(0)
+    let time = new Date();
 
-            setInterval(function () {
-                time.setSeconds(time.getSeconds() + 1)
-                tempoAttuale.innerHTML = time.toLocaleTimeString()
-            }, 1000)
-    
+    time.setHours(0)
+    time.setMinutes(0)
+    time.setSeconds(0)
 
-    let arrayComparison = [];
+    interval = setInterval(function () {
+        time.setSeconds(time.getSeconds() + 1)
+        tempoAttuale.innerHTML = 'Tempo: ' + time.getMinutes() + ' min ' + time.getSeconds() + ' sec'
+    }, 1000)
+
     var arrayShuffle = shuffle(arrayAnimali);
     let lista = document.querySelector('#griglia');
     lista.innerHTML = ''
 
-    
     for (elemento of arrayShuffle) {
-
         let boxEsterno = document.createElement('div');
-        let boxInterno = document.createElement('div');
-
-        boxInterno.classList.add('icon');
-
+        let icon = document.createElement('div');
+        icon.classList.add('icon');
         lista.append(boxEsterno);
-        boxEsterno.append(boxInterno);
-
-        boxInterno.innerHTML = elemento;
-       
+        boxEsterno.append(icon);
+        icon.innerHTML = elemento;
+        icon.addEventListener("click", displayIcon)
     }
-
-
-
 }
-
-
 // la funzione startGame che pulisce il timer, dichiara un array vuoto, mescola casualmente l'array degli animali
 // (var arrayShuffle = shuffle(arrayAnimali);), aggancia il contenitore con id griglia, 
 // pulisce tutti gli elementi che eventualmente contiene
 // poi fa ciclo per creare i 24 div child -> aggiunge la class e l'elemento dell'array in base all'indice progressivo
 // chiama la funzione timer e associa a tutti gli elementi (div) di classe icon l'evento click e le due funzioni definit sotto
-
-
-
 function displayIcon() {
-    var icon = document.getElementsByClassName("icon");
-    var icons = [...icon];
 
-    /*
     var icon = document.getElementsByClassName("icon");
     var icons = [...icon];
-    è uguale a 
-    var icons = document.getElementsByClassName("icon");
-    //var icons = [...icon];
-    è un operatore che serve per passare un array come argomento:
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax 
-    https://www.tutorialspoint.com/es6/es6_operators.htm (cerca spread nella pagina)
-    */
 
     //mette/toglie la classe show
+    //mio commento: toggla anche disabled altrimenti potrei aggiungere la stessa icona all'array di convalida scatenando un falso positivo.
     this.classList.toggle("show");
+    this.classList.toggle("disabled");
+
     //aggiunge l'oggetto su cui ha cliccato all'array del confronto
+    //mio commento: in fase di assemblaggio del codice, mi sono servito dei console.log per capire il comportamento delle funzioni e dei fix dei problemi
     arrayComparison.push(this);
+    console.log(arrayComparison);
 
     var len = arrayComparison.length;
+    var victory = victoryArray.length;
+
     //se nel confronto ci sono due elementi
     if (len === 2) {
         //se sono uguali aggiunge la classe find
+        //mio commento: ho rimosso QUASI tutto il codice in dotazione, e l'ho rifatto seguendo una logica diversa.
         if (arrayComparison[0].innerHTML === arrayComparison[1].innerHTML) {
             arrayComparison[0].classList.add("find", "disabled");
             arrayComparison[1].classList.add("find", "disabled");
             arrayComparison = [];
-        } else {
-            //altrimenti (ha sbagliato) aggiunge solo la classe disabled
-            icons.forEach(function (item) {
-                item.classList.add('disabled');
-            });
-            // con il timeout rimuove  la classe show per nasconderli
+            victoryArray.push("count");
+            console.log(victoryArray);
+            console.log(victoryArray.length);
+        }
+        else {
             setTimeout(function () {
-                arrayComparison[0].classList.remove("show");
-                arrayComparison[1].classList.remove("show");
-                icons.forEach(function (item) {
-                    item.classList.remove('disabled');
-                    for (var i = 0; i < iconsFind.length; i++) {
-                        iconsFind[i].classList.add("disabled");
-                    }
-                });
+                arrayComparison[0].classList.remove("show", "disabled");
+                arrayComparison[1].classList.remove("show", "disabled");
                 arrayComparison = [];
-            }, 700);
+            }, 800)
+        }
+
+        if (victory == 11) {
+            console.log("victory");
+            let timer = document.getElementById("tempoTrascorso");
+            timer.innerHTML = tempoAttuale.innerHTML;
+            gameCompleted.style.display = 'visible';
+            gameCompleted.classList.add('active');
         }
     }
 }
+
+// con il timeout rimuove  la classe show per nasconderli
+
+// mio commento: ho riorganizzato il timeout per fixare l'animazione della seconda icona cliccata dandogli un delay di 800ms, compattando il codice in poche righe.
+// la stessa cosa avviene per le consegne sotto. Il codice e' stato scritto per come ci ragionavo, ammetto di non essere stato in grado di
+// integrare il codice della consegna: l'ho trovato complicato per come lo stavo ragionando io e non riuscivo a comprenderlo in relazione a come lo stavo impostando sulla funzione playGame.
+
 
 //una funzione che viene mostrata alla fine quando sono tutte le risposte esatte
 
