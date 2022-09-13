3acd595d683591fbcf235b1d18efe7a16417e6ed
Nabbatrox <66868366+Nabbatrox@users.noreply.github.com>
2022-09-13 19:50:48 +0200

    esercizi 1 e 2

---
+++ m3/w3/d1/consegna/angular1/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PhotoInterceptor } from './photo.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    
+  ],
+  providers: [{  provide:HTTP_INTERCEPTORS,
+    useClass:PhotoInterceptor,
+    multi: true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ m3/w3/d1/consegna/angular1/src/app/photo.ts
@@ -0,0 +1,14 @@
+export class Photo {
+
+title: string
+url: string
+id: number | undefined 
+
+constructor(title: string, url: string) {
+
+this.title = title
+this.url = url
+}
+
+
+}
